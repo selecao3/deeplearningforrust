4ebbaa4d4144bb12ad2d0fc0693e98ca04ba1db5
kazuha <mami.tomoe4@gmail.com>
2019-04-05 10:28:44 +0900

    for windows builded

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-use std::collections::HashMap;
-
-fn preprocess(text: String) {
-    let text = text.to_lowercase();
-    let text = text.replace(".", " .");
-    let words: Vec<&str> = text.split(" ").collect();
-    let mut word_to_id: HashMap<&str, i32> = HashMap::new();
-    let mut id_to_word: HashMap<i32, &str> = HashMap::new();
-    for word in words.iter() {
-        if word != word_to_id.keys() {
-            unimplemented!();
-        }
-    }
-}
-
 fn main() {
-    let text = "I'm kazuha Ishida.";
-    preprocess(text.to_string());
+    println!("hello Rust!");
 }
