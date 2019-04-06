6b93f5f8f9e9c93c73a4feb62438c7d65233a212
kazuha <mami.tomoe4@gmail.com>
2019-04-06 22:57:14 +0900

    strからstringへ(6 lines)

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use std::collections::HashMap;
 
-fn preprocess(text: String) {
+fn preprocess(text: &str) -> (HashMap<String, usize>, HashMap<usize, String>) {
     let mut new_id: usize;
-    let text = text.to_lowercase();
-    let text = text.replace(".", " .");
-    let words: Vec<&str> = text.split(" ").collect();
-    let mut word_to_id: HashMap<&str, usize> = HashMap::new();
-    let mut id_to_word: HashMap<usize, &str> = HashMap::new();
+    let text = text.to_lowercase().replace(".", " .");
+    let words: Vec<String> = text.split(" ").map(|str: &str| str.to_string()).collect();
+    let mut word_to_id: HashMap<String, usize> = HashMap::new();
+    let mut id_to_word: HashMap<usize, String> = HashMap::new();
     for word in words {
-        if word_to_id.keys().find(|&&x| x == word).is_none() {
+        if word_to_id.keys().find(|&x| x == &word).is_none() {
             new_id = word_to_id.len();
-            word_to_id.insert(word, new_id);
-            id_to_word.insert(new_id, word);
+            word_to_id.insert(word.clone(), new_id);
+            id_to_word.insert(new_id, word.clone());
         }
     }
     println!("{:?}", word_to_id);
     println!("{:?}", id_to_word);
+    return (word_to_id, id_to_word);
 }
 
 fn main() {
     let text = "I'm kazuha Ishida.";
-    preprocess(text.to_string());
+    preprocess(text);
 }
