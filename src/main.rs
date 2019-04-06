978830060f5487f0c25f58e0d5ea45b3438802cf
kazuha <mami.tomoe4@gmail.com>
2019-04-06 18:39:39 +0900

    67 page

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-fn main() {
-    println!("リモート実行テスト");
+use std::collections::HashMap;
 
+fn preprocess(text: String) {
+    let mut new_id: usize;
+    let text = text.to_lowercase();
+    let text = text.replace(".", " .");
+    let words: Vec<&str> = text.split(" ").collect();
+    let mut word_to_id: HashMap<&str, usize> = HashMap::new();
+    let mut id_to_word: HashMap<usize, &str> = HashMap::new();
+    for word in words {
+        if word_to_id.keys().find(|&&x| x == word).is_none() {
+            new_id = word_to_id.len();
+            word_to_id.insert(word, new_id);
+            id_to_word.insert(new_id, word);
+        }
+    }
+}
+
+fn main() {
+    let text = "I'm kazuha Ishida.";
+    preprocess(text.to_string());
 }
--- target/debug/win_target_sample.d
+++
@@ -1,1 +0,0 @@
-/Users/kazuha/develop/Rust/win_target_sample/target/debug/win_target_sample.dSYM: /Users/kazuha/develop/Rust/win_target_sample/src/main.rs
