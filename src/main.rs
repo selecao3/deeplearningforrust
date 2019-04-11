633086afd6325dfc9e126f3158cc7b8bd3f1320c
kazuha <mami.tomoe4@gmail.com>
2019-04-12 00:10:47 +0900

    標準ライブラリのみに修正

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
-extern crate ndarray;
-extern crate ndarray_linalg;
-use ndarray::Array;
-use ndarray::Array2;
-use ndarray_linalg::*;
 use std::collections::HashMap;
 
-fn preprocess(
-    text: &str,
-) -> (
-    Array<usize, ndarray::Dim<[usize; 1]>>,
-    HashMap<String, usize>,
-    HashMap<usize, String>,
-) {
+fn preprocess(text: &str) -> (Vec<usize>, HashMap<String, usize>, HashMap<usize, String>) {
     let mut new_id: usize;
-    let mut id_vec: Vec<usize> = Vec::new();
+    let mut corpus: Vec<usize> = Vec::new();
     let mut word_to_id: HashMap<String, usize> = HashMap::new();
     let mut id_to_word: HashMap<usize, String> = HashMap::new();
     let text = text.to_lowercase().replace(".", " .");
@@ -22,23 +11,19 @@ fn preprocess(
         if word_to_id.keys().find(|&x| x == &word).is_none() {
             new_id = word_to_id.len();
             word_to_id.insert(word.clone(), new_id);
-            id_to_word.insert(new_id, word);
-            id_vec.push(new_id);
+            id_to_word.insert(new_id, word.clone());
         }
+        corpus.push(word_to_id[&word]);
     }
-    let corpus = Array::from_vec(id_vec);
     return (corpus, word_to_id, id_to_word);
 }
 
-fn create_co_matrix(
-    corpus: Array<usize, ndarray::Dim<[usize; 1]>>,
-    vocab_size: usize,
-) -> Array<usize, ndarray::Dim<[usize; 2]>> {
+fn create_co_matrix(corpus: Vec<usize>, vocab_size: usize) -> Vec<Vec<usize>> {
     let windows_size: usize = 1;
     let corpus_size: usize = corpus.len();
-    let mut co_matrix = Array2::<usize>::zeros((vocab_size, vocab_size));
+    let mut co_matrix = vec![vec![0; vocab_size]; vocab_size];
 
-    for (idx, word_id) in corpus.indexed_iter() {
+    for (idx, word_id) in corpus.iter().enumerate() {
         for i in 1..windows_size + 1 {
             let left_idx: usize;
             let right_idx: usize = idx + i;
@@ -47,13 +32,15 @@ fn create_co_matrix(
             if idx != 0 {
                 left_idx = idx - i;
                 if let Some(left_word_id) = corpus.get(left_idx) {
-                    co_matrix[[word_id, left_word_id.clone()]] += 1; //word_idとleft_word_idの型が一致していないとだめ＆参照だとダメ（left_word_id）
+                    //co_matrix[[word_id, left_word_id.clone()]] += 1; //word_idとleft_word_idの型が一致していないとだめ＆参照だとダメ（left_word_id）
+                    co_matrix[word_id][left_word_id.clone()] += 1; //word_idとleft_word_idの型が一致していないとだめ＆参照だとダメ（left_word_id）
                 }
             }
 
             if right_idx < corpus_size {
                 if let Some(right_word_id) = corpus.get(right_idx) {
-                    co_matrix[[word_id, right_word_id.clone()]] += 1;
+                    //co_matrix[[word_id, right_word_id.clone()]] += 1;
+                    co_matrix[word_id][right_word_id.clone()] += 1;
                 }
             }
         }
@@ -63,18 +50,30 @@ fn create_co_matrix(
 }
 
 // 引数（usize）=> mapvでf32にキャスト=>norm_l1()
-fn cos_similarity(
-    x: Array<usize, ndarray::Dim<[usize; 1]>>,
-    y: Array<usize, ndarray::Dim<[usize; 1]>>,
-) -> () {
+fn cos_similarity(x: Vec<usize>, y: Vec<usize>) -> () {
     let eps = 0.00000001;
-    let float_x = x.mapv(|tmp: usize| tmp as f32);
-    let float_y = y.mapv(|tmp: usize| tmp as f32);
+    let float_x: Vec<f32> = x.iter().map(|&tmp| tmp as f32).collect();
+    let float_y: Vec<f32> = y.iter().map(|&tmp| tmp as f32).collect();
     //let nx = float_x.clone() / float_x.mapv(|tmp: f32| tmp * tmp + eps).norm_l2();
-    let nx = float_x.clone() / float_x.mapv(|tmp: f32| tmp + eps).norm_l2();
+    let norm = |vec: Vec<f32>| -> f32 {
+        let vec_pow2 = vec.iter().map(|x| x * x);
+        let vec_sum: f32 = vec_pow2.sum();
+        vec_sum.sqrt()
+    };
+    //let nx = float_x.clone() / norm(float_x);
+    let nx: Vec<f32> = float_x
+        .clone()
+        .iter()
+        .map(|x| x / (norm(float_x.clone()) + eps))
+        .collect();
+    let ny: Vec<f32> = float_y
+        .clone()
+        .iter()
+        .map(|y| y / (norm(float_y.clone()) + eps))
+        .collect();
     //let ny = float_y.clone() / float_y.mapv(|tmp: f32| tmp * tmp + eps).norm_l2();
-    let ny = float_y.clone() / float_y.mapv(|tmp: f32| tmp + eps).norm_l2();
-    let hoge = ny.dot(&nx);
+    //let ny = float_y.clone() / float_y.mapv(|tmp: f32| tmp + eps).norm_l2();
+    let hoge: f32 = ny.iter().zip(nx.iter()).map(|(x, y)| x * y).sum();
     println!("{:?}", hoge);
 }
 
@@ -85,17 +84,19 @@ fn main() {
     vocab_size = word_to_id.len();
     println!("{:?}", word_to_id);
     println!("{:?}", id_to_word);
+    println!("{:?}", corpus);
 
     let C = create_co_matrix(corpus, vocab_size);
-    let mut c0 = Array::default(1);
-    let mut c1 = Array::default(1);
+    let mut c0 = vec![0; vocab_size];
+    let mut c1 = vec![0; vocab_size];
     if let Some(target_word) = word_to_id.get("you") {
-        c0 = C.row(target_word.clone()).to_owned();
+        let target_word = target_word.clone();
+        c0 = C[target_word].to_owned();
         println!("c0");
         println!("{:?}", c0);
     }
     if let Some(target_word) = word_to_id.get("goodbye") {
-        c1 = C.row(target_word.clone()).to_owned();
+        c1 = C[target_word.clone()].to_owned();
     }
     cos_similarity(c0, c1);
 }
